3f2f1df5d20a92bc423471dac0cc6577babac63f
the-cicero <109515808+the-cicero@users.noreply.github.com>
2022-07-19 15:19:13 +0300

    Create msuteri.cs

---
+++ SınıfMetotDemo/msuteri.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    internal class Musteri
+    {
+        public int Id { get; set; }
+        public string Adi { get; set; }
+        public string Soyadi { get; set; }
+        public double TcKimlikNo { get; set; }
+        public int KrediPuani { get; set; }
+        
+    }
+}
